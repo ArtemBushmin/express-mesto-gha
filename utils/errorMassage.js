1eef2abae7854eff05c6e0284b1a2ecce22c517e
Artem Bushmin <nim-shub@rambler.ru>
2022-07-28 22:45:15 +0300

    Работа готова тесты3

--- utils/errorMassage.js
+++ utils/errorMassage.js
@@ -7,7 +7,7 @@ function errorMessage(err, res) {
     return res.status(400).send({ message: err.message });
   }
 
-  if (err.name === 'DocumentNotFoundError') {
+  if (err.name === 'NotFoundError') {
     return res.status(404).send({ message: err.message });
   }
 
