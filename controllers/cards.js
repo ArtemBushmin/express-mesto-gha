3c98ba6d887d1c61bf72ccf8bab91f8586ea7335
Artem Bushmin <nim-shub@rambler.ru>
2022-07-29 22:05:13 +0300

    Ревью1

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,22 +1,38 @@
 const Card = require('../models/card');
-const { errorMessage } = require('../utils/errorMassage');
 
 module.exports.getCards = (req, res) => {
-  Card.find({}).then((cards) => res.send(cards)).catch((err) => errorMessage(err, res));
+  Card.find({})
+    .then((cards) => res.send(cards))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
-    .catch((err) => errorMessage(err, res));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
     .orFail()
     .then((card) => res.send(card))
-    .catch((err) => errorMessage(err, res));
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.likeCard = (req, res) => {
@@ -25,7 +41,18 @@ module.exports.likeCard = (req, res) => {
     req.params.id,
     { $addToSet: { likes: req.user._id } },
     { new: true },
-  ).orFail().then((card) => res.send(card)).catch((err) => errorMessage(err, res));
+  )
+    .orFail()
+    .then((card) => res.send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: err.message });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -33,5 +60,16 @@ module.exports.dislikeCard = (req, res) => {
     req.params.id,
     { $pull: { likes: req.user._id } },
     { new: true },
-  ).orFail().then((card) => res.send(card)).catch((err) => errorMessage(err, res));
+  )
+    .orFail()
+    .then((card) => res.send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: err.message });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,38 @@
 const User = require('../models/user');
-const { errorMessage } = require('../utils/errorMassage');
 
 module.exports.getUsers = (req, res) => {
-  User.find({}).then((users) => res.send(users)).catch((err) => errorMessage(err, res));
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.getUserById = (req, res) => {
   const { id } = req.params;
-  User.findById(id).orFail().then((user) => res.send(user)).catch((err) => errorMessage(err, res));
+  User.findById(id).orFail()
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
-    .catch((err) => errorMessage(err, res));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.updateUserProfile = (req, res) => {
@@ -22,7 +40,15 @@ module.exports.updateUserProfile = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .orFail()
     .then((user) => res.send(user))
-    .catch((err) => errorMessage(err, res));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: err.message });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
@@ -30,5 +56,13 @@ module.exports.updateUserAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .orFail()
     .then((user) => res.send(user))
-    .catch((err) => errorMessage(err, res));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: err.message });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
